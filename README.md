8a2099736915d6ba85e485fbc649b4e59b667eb5
Rohit Kochikkat Francis <rohit.kf.dev@gmail.com>
2024-04-22 14:31:09 +0100

    Update README, requirements and app

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Updated-Langchain
+# Introduction
+A simple implementation of RAG using open source models
+Checkout my other projects [here](https://rohitkf.dev)
+
+# Steps to run the application :
+- Step 0 : Create a new environment using [Anaconda](https://www.anaconda.com/download)
+    ```Bash
+    conda create -p venv -y
+    ```
+
+- Step 1 : Install Ollama [here](https://ollama.com/), then install the model
+    ```
+    ollama run llama2
+    ```
+- Step 2 : Install the dependencies
+    ```Bash
+    pip install -r requirements.txt
+    ```
+- Step 3 : Create `.env` file with API KEY from [groq](https://console.groq.com/keys)
+    ```Bash
+    GROQ_API_KEY=your_api_key
+    ```
+- Step 4 : Run the application
+    ```Bash
+    streamlit run app.py
+    ```
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -16,14 +16,18 @@ load_dotenv()
 ## load the Groq API key
 groq_api_key=os.environ['GROQ_API_KEY']
 
-if "vector" not in st.session_state:
-    st.session_state.embeddings=OllamaEmbeddings()
-    st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
-    st.session_state.docs=st.session_state.loader.load()
+url = st.text_input("Input you URL here", value="https://docs.smith.langchain.com/")
 
-    st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
-    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
-    st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+if url:
+    if "vector" not in st.session_state:
+        st.session_state.embeddings=OllamaEmbeddings()
+        # st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
+        st.session_state.loader=WebBaseLoader(url)
+        st.session_state.docs=st.session_state.loader.load()
+
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
 
 st.title("ChatGroq Demo")
 llm=ChatGroq(groq_api_key=groq_api_key,
